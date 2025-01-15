b38b5154a3b79d970397994ad5392e552698a632
Nick-Chan <nick.chan@williams.uk.com>
2025-01-15 16:52:35 +0000

    feat: added different region button filters in the frontend, for the backend added API get request for each region

--- Services/RiotApiService.cs
+++ Services/RiotApiService.cs
@@ -15,9 +15,22 @@ namespace TFT.Services
             _apiKey = "RGAPI-cec83fcf-0f13-44c7-b3b1-d005818c039b"; // API Key
         }
 
+        // REGIONS: BR1, EUN1, EUW1, JP1, KR, LA1, LA2, ME1, NA1, OC1, PH2, RU, SG2, TH2, TR1, TW2, VN2
+        List<string> regions = new List<string>
+        {
+            "BR1", "EUN1", "EUW1", "JP1", "KR", "LA1", "LA2", "ME1", "NA1",
+            "OC1", "PH2", "RU", "SG2", "TH2", "TR1", "TW2", "VN2"
+        };
+
+
         public async Task<string> GetTopPlayersAsync()
         {
-            string url = $"https://euw1.api.riotgames.com/tft/league/v1/challenger?queue=RANKED_TFT&api_key={_apiKey}";
+            foreach (var region in regions)
+            {
+                string urls = $"https://{region}.api.riotgames.com/tft/league/v1/challenger?queue=RANKED_TFT&api_key={_apiKey}";
+            }
+            
+            string url = $"https://EUW1.api.riotgames.com/tft/league/v1/challenger?queue=RANKED_TFT&api_key={_apiKey}";
 
             var response = await _httpClient.GetAsync(url);
             response.EnsureSuccessStatusCode();
@@ -27,6 +40,11 @@ namespace TFT.Services
 
         public async Task<string> GetSummonerDetailsAsync(string summonerId)
         {
+            foreach (var region in regions)
+            {
+                string urls = $"https://{region}.api.riotgames.com/tft/summoner/v1/summoners/{summonerId}?api_key={_apiKey}";
+            }
+
             string url = $"https://euw1.api.riotgames.com/tft/summoner/v1/summoners/{summonerId}?api_key={_apiKey}";
 
             var response = await _httpClient.GetAsync(url);
